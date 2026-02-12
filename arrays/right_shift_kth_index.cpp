128dfa2e5b996a782ba2ae19741a6cc8486d1972
Udhay Chaudhary <udhay2007ch@gmail.com>
2026-02-13 00:22:03 +0530

    Added right shifting by kth index program

---
+++ arrays/right_shift_kth_index.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"n: ";
+    cin>>n;
+
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int k;
+    cout<<"k: ";
+    cin>>k;
+
+    k = k % n;   
+
+    while(k--){
+        int last = arr[n-1];
+
+        for(int i=n-1; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+
+        arr[0] = last;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
