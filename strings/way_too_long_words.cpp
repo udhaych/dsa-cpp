fb3ddfe601ca2b47aa484657b221c921aa211d16
Udhay Chaudhary <udhay2007ch@gmail.com>
2026-02-13 00:58:25 +0530

    Added Codeforces 71A - Way Too Long Words (Strings)

---
+++ strings/way_too_long_words.cpp
@@ -0,0 +1,30 @@
+// Platform: Codeforces
+// Problem: 71A - Way Too Long Words
+// Topic: Strings
+// Difficulty: Easy
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+
+    while(n--){
+
+        string s;
+        cin>>s;
+
+        if(s.length()<=10){
+            cout << s << endl;
+        }
+
+        else{
+            cout << s[0] << s.length()-2 << s[s.length()-1] << endl; 
+        }
+    }
+
+    return 0;
+}
